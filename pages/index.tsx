43e2e64f7bf4f989fea1f4295eafb9603a848290
Gabriel McReynolds <gabrielmc785@gmail.com>
2022-11-11 21:45:52 -0600

    got basic appwrite working

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,29 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import {Client, Databases, Models} from "appwrite";
+import {GetServerSideProps} from "next";
 
 
-export default function Home() {
-  return (
-    <>
-      <h1 className="text-3xl">Hello world</h1>
-    </>
-  )
+export default function Home({documents}: { documents: Models.DocumentList<Models.Document> }) {
+    return (
+        <>
+            <ul>
+                {documents.documents.map((doc) =>
+                    <li key={doc.$id}>
+                        {doc.name} -- {doc.$id}
+                    </li>)}
+            </ul>
+        </>
+    )
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const client = new Client()
+        .setEndpoint('http://localhost/v1')
+        .setProject('636f1031153aa3b07928');
+
+    const databases = new Databases(client);
+
+    const documents = await databases.listDocuments('636f12040b4dcf85b62c', '636f120814bc273d82db');
+    return {
+        props: {documents},
+    }
 }
